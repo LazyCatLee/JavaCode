505392d1c36a6693ff7dd3b70d121404be5fb489
Lee <1277758193@qq.com>
2017-05-22 18:53:24 +0800

    选择排序法

---
+++ SortText.java
@@ -0,0 +1,32 @@
+public class SortTest{
+	public static void main(String[] args){
+		int[] intarray = {1,34,45,67,65,23,6,676,56,5};
+		int keyvalue;
+		int index;
+		int temp;
+
+		System.out.println("排序前的数组元素");
+		for(int a : intarray){
+			System.out.print(a+" ");
+		}
+		System.out.println("-----------------");
+		for(int i = 0;i <intarray.length;i++){
+			index=i;
+			keyvalue=intarray[i];
+			for(int j = 0;j<intarray.length;j++){
+				if(intarray[j]<keyvalue){
+					index=j;
+					keyvalue=intarray[j];
+				}
+			}
+			temp=intarray[i];
+			intarray[i]=intarray[index];
+			intarray[index]=temp;
+		}
+		System.out.println("排序后的数组：");
+		for(int b:intarray){
+			System.out.println(b+ " ");
+		}
+	}
+
+}

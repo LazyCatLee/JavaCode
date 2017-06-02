54f0b393823adde1071f750850b2d2e585c7fdee
Lee <1277758193@qq.com>
2017-06-02 09:39:13 +0800

    变量初始化跟内存分配的先后证明

--- 17.6.1/Test.java
+++ 17.6.1/Test.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-
+//变量的初始化跟内存分配
 class Test {
 
 	int x = getX();
-	
+	int y=2;
 	public int getX(){
-		System.out.println("初始化先执行");
+		System.out.println(x);
+		System.out.println(y);
 		return 1;
 	}
-	int y=1;
+	
 	public static void main(String[] args) {
 	Test t = new Test();
 	System.out.println("X="+t.x);
 	System.out.println("Y="+t.y);
 
-	Test ts = new Test();
-	System.out.println("Y="+ts.y);
-	System.out.println("X="+ts.x);
+	//Test ts = new Test();
+	//System.out.println("Y="+ts.y);
+	//.out.println("X="+ts.x);
 	}
 
 }
